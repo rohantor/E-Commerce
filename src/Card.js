f992a4b0b0fc544c8fdb6b6b9f468bdf6edcfaab
rohantor <rohan.magar@torinit.ca>
2023-01-05 11:08:24 +0530

    added delete icon

--- src/Card.js
+++ src/Card.js
@@ -1,60 +1,70 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import Modal from './Modal'
-import ModalTest from './ModalTest';
+import React from "react";
+import Button from "@mui/material/Button";
+import Modal from "./Modal";
+import ModalTest from "./ModalTest";
 export default function Card(props) {
-    const {title,url,description,id} = props.Product;
-    const [deleteStatus , setDeletestatus]= React.useState(props.Product.deleteStatus);
-    const  [ modalPos , setModalPos] = React.useState(false);
-    
-  return (<>
-  
+  const { title, url, description, id } = props.Product;
+  const [deleteStatus, setDeletestatus] = React.useState(
+    props.Product.deleteStatus
+  );
+  const [modalPos, setModalPos] = React.useState(false);
 
-  
-    <div className='card_outer' >
-        
-        <div >
-             <h3 className='title' style={{display:"inline-block"}}>
+  return (
+    <>
+      <div className="card_outer">
+        <div>
+          <h3 className="title" style={{ display: "inline-block" }}>
+            {id}
+            <br />
+            {title}
+          </h3>
+
+          
             
-             {id}
-            <br />{title}
+            <img
+              src="/trash.png"
+              alt=""
+              onClick={() => {
+                setDeletestatus(!deleteStatus);
+                console.log("Delete");
+              }}
+              style={{ display: "inline-block", heigth: "16px"}}
+            />
            
-        </h3>
-        <button  style={{display:"inline-block"}}
-         onClick={()=>{
-
-            setDeletestatus(!deleteStatus)
-            console.log("Delete")
-
-        }}>*</button>
         </div>
         {/* <Modal Product ={props.Product}/> */}
-        <img src={url}
-        onClick={()=>{setModalPos(old=>!old)}}
-        className='Card_img'/>
-        {
-            deleteStatus?( <Button variant="outlined" color="error" onClick={()=>{
-                    props.Product.setRemoved(old=>[...old,id])
-                    console.log("Deleted")
-            }
-            }
-             >
+        <img
+          src={url}
+          onClick={() => {
+            setModalPos((old) => !old);
+          }}
+          className="Card_img"
+        />
+        {deleteStatus ? (
+          <Button
+            variant="outlined"
+            color="error"
+            onClick={() => {
+              props.Product.setRemoved((old) => [...old, id]);
+              console.log("Deleted");
+            }}
+          >
             Delete
-        </Button >)
-            :<>
-            <Button variant="contained">
-            WishList
-        </Button>
-        <Button variant="outlined">
-            Add to cart
-        </Button></>
-        }
-       
-        <p>
-           {description}
-        </p>
-    </div>
-    <ModalTest isClose ={modalPos} setModalPos ={setModalPos} Product={props.Product}></ModalTest>
+          </Button>
+        ) : (
+          <>
+            <Button variant="contained">WishList</Button>
+            <Button variant="outlined">Add to cart</Button>
+          </>
+        )}
+
+        <p>{description}</p>
+      </div>
+      <ModalTest
+        isClose={modalPos}
+        setModalPos={setModalPos}
+        Product={props.Product}
+      ></ModalTest>
     </>
-  )
+  );
 }
